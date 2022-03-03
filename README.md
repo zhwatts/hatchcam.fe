23224610bea3b136b21ffc598317700ac6beb8e7
Zach Watts <zach@zachwatts.online>
2022-03-03 17:24:25 -0500

    init commit. CRA react boot strap with logo only landing page

---
+++ README.md
@@ -0,0 +1,11 @@
+<!-- @format -->
+
+# HatchCam.net
+
+A **STEM** focused platform for monitoring bird nesting habits and plant growth
+</br>
+</br>
+
+## This repo is the HatchCam.Net React app
+
+This repo constitutes the react portion of the HatchCam.Net web application
---
+++ src/App.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import logo from "./hatchcam_logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+		</div>
+	);
+}
+
+export default App;
